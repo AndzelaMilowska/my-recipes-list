a70eb4437d112bccbcb98c84bbcb63fe9f4361a7
Nela <andzelamilowska@gmail.com>
2024-05-03 18:05:22 +0200

    Add single recipe view

--- src/app/recipes/recipe/recipe.component.ts
+++ src/app/recipes/recipe/recipe.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss',
 })
-export class RecipeComponent {}
+export class RecipeComponent implements OnInit {
+  constructor(private recipes: RecipesService) {}
+  activeRecipe = this.recipes.recipes[0]; //placeholder data
+  initialPortions: number = this.activeRecipe.numberOfPortions;
+
+  ngOnInit(): void {}
+}
+
+//this should be single recipe display component
+//create another recipe edit for edditing and adding recipe
+//add WYSIWYG to recipe editor?? --> storing all text data, not just a string
